7499f35ef99ce8998b73466acf9e05e7f5fc4cdb
Moreau Matthias <moreau.matthias86@gmail.com>
2021-04-27 09:23:43 +0200

    test1

---
+++ app/src/main/java/com/example/test2/Apprenti.java
@@ -0,0 +1,18 @@
+package com.example.test2;
+
+import java.util.Date;
+
+public class Apprenti {
+
+    private String nomApp;
+    private String prenomApp;
+    private String adresseApp;
+    private String telApp;
+    private Date dateDebutApp;
+    private Classe laClasse;
+    private String mailApp;
+
+    public Apprenti(String nom, String prenom, String adresse, String tel, Date dateDebut, Classe uneclasse, String mail){
+
+    }
+}
---
+++ app/src/main/java/com/example/test2/Classe.java
@@ -0,0 +1,7 @@
+package com.example.test2;
+
+public class Classe {
+
+    private String libelle;
+
+}
